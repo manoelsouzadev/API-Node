a8cb8b52a60bb8504052ec9b2e50075680711e36
manoelsouzadev <manoel.souza280@gmail.com>
2019-09-02 20:45:39 -0300

    API Node em desenvolvimento

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const app = express();
+
+//Conencta ao banco
+mongoose.connect("mongodb+srv://manoel:mongodbnosql@cluster0-4lzxg.mongodb.net/test?retryWrites=true&w=majority");
+//Carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", indexRoute);
+app.use("/products", productRoute);
+
+module.exports = app;
