da96404cb2fa3176b922c0cb604f4aa10582465d
manoelsouzadev <manoel.souza280@gmail.com>
2019-09-03 11:10:58 -0300

    Adicionado find pelo slug no product-controller

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,13 +4,21 @@ const mongoose = require("mongoose");
 const Product = mongoose.model("product");
 
 exports.get = (req, res, next) => {
-  Product.find({ active: true }, "title price slug" ).then(data => {
+  Product.find({ active: true }, 'title price slug' ).then(data => {
     res.status(200).send(data);
 
   }).catch(e => {
     res.status(400).send(e);;
   });
 };
+
+exports.getBySlug = ((req, res, next) => {
+  Product.findOne({ slug: req.params.slug, active: true }, 'title description price slug tags').then( data => {
+    res.status(200).send( data );
+  }).catch( e => {
+    res.status(400).send( e );
+  })
+});
 
 
 exports.post = (req, res, next) => {
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const productController = require('../controllers/product-controller');
 
 router.get('/', productController.get);
+router.get('/:slug', productController.getBySlug);
 router.post("/", productController.post);
 router.put("/:id", productController.put);
 router.delete("/", productController.delete);
