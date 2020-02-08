4426b5f5b560ddc3ea92cfcd04f7506657cfdedb
manoelsouzadev <manoel.souza280@gmail.com>
2020-02-08 13:04:45 -0300

    Funcionalidade do order finalizada

---
+++ src/routes/order-route.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/order-controller');
+
+router.get('/', orderController.get);
+router.post('/', orderController.post);
+
+module.exports = router;
