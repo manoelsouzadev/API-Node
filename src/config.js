044b316917fa79f3461756bb978c95416cff6484
manoelsouzadev <manoel.souza280@gmail.com>
2020-02-09 17:57:59 -0300

    Adicionado service para envio de email

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong> {0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
   connectionString: 'mongodb+srv://manoel:mongodbnosql@cluster0-4lzxg.mongodb.net/test?retryWrites=true&w=majority',
-  sendgridKey: 'TBD',
+  sendgridKey: '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',
   containerConnectionString: 'TBD'
 };
--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -4,44 +4,49 @@ const validationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
 const md5 = require('md5');
 
+const emailService = require('../services/email-service');
+
 exports.post = async (req, res, next) => {
-    let contract = new validationContract();
-    contract.hasMinLen(
-      req.body.name,
-      3,
-      'O nome deve conter pelo menos 3 caracteres'
-    );
-    contract.isEmail(
+  let contract = new validationContract();
+  contract.hasMinLen(
+    req.body.name,
+    3,
+    'O nome deve conter pelo menos 3 caracteres'
+  );
+  contract.isEmail(req.body.email, 'E-mail inválido');
+  contract.hasMinLen(
+    req.body.password,
+    3,
+    'A senha deve conter pelo menos 6 caracteres'
+  );
+
+  if (!contract.isValid()) {
+    res
+      .status(400)
+      .send(contract.errors())
+      .end();
+    return;
+  }
+
+  try {
+    await repository.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: md5(req.body.password + global.SALT_KEY)
+    });
+
+    emailService.send(
       req.body.email,
-      'E-mail inválido'
+      'Bem vindo ao node store',
+      global.EMAIL_TMPL.replace('{0}', req.body.name)
     );
-    contract.hasMinLen(
-      req.body.password,
-      3,
-      'A senha deve conter pelo menos 6 caracteres'
-    );
-  
-    if (!contract.isValid()) {
-      res
-        .status(400)
-        .send(contract.errors())
-        .end();
-      return;
-    }
-  
-    try {
-      await repository.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: md5(req.body.password + global.SALT_KEY)
-      });
-      res.status(201).send({
-        message: 'Cliente cadastrado com sucesso!'
-      });
-    } catch (e) {
-      res.status(500).send({
-        message: 'Falha ao processar sua requisição'
-      });
-    }
-  };
-  
+
+    res.status(201).send({
+      message: 'Cliente cadastrado com sucesso!'
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: 'Falha ao processar sua requisição'
+    });
+  }
+};
