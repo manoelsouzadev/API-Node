661c0ffbeea631a1d4f8ab8efd51686e78a5fb71
manoelsouzadev <manoel.souza280@gmail.com>
2020-02-09 17:10:49 -0300

    Adicionado encriptacao da senha

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -2,6 +2,7 @@
 
 const validationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
+const md5 = require('md5');
 
 exports.post = async (req, res, next) => {
     let contract = new validationContract();
@@ -29,7 +30,11 @@ exports.post = async (req, res, next) => {
     }
   
     try {
-      await repository.create(req.body);
+      await repository.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: md5(req.body.password + global.SALT_KEY)
+      });
       res.status(201).send({
         message: 'Cliente cadastrado com sucesso!'
       });
