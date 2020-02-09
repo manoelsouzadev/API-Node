bb4fec175fab26078703bb941a5a1911dfaf49f1
Manoel Souza <48496686+manoelsouzadev@users.noreply.github.com>
2020-02-09 19:25:50 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong> {0}</strong>, seja bem vindo à Node Store!'
 
 module.exports = {
   connectionString: 'mongodb+srv://manoel:mongodbnosql@cluster0-4lzxg.mongodb.net/test?retryWrites=true&w=majority',
-  sendgridKey: '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',
+  sendgridKey: '',
   containerConnectionString: 'TBD'
 };
