3af5d8b5cd1f8ff4103f6c403218415bf97fbe15
manoelsouzadev <manoel.souza280@gmail.com>
2020-03-23 15:59:01 -0300

    Adicionado funcionalidade refreh token no customer

--- src/routes/customer-route.js
+++ src/routes/customer-route.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customer-controller');
+const authService = require('../services/auth-service');
 
 router.post('/', customerController.post);
 router.post('/authenticate', customerController.authenticate);
+router.post('/refresh-token', authService.authorize, customerController.refreshToken);
 
 module.exports = router;
